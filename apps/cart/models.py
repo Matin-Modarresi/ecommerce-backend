33a342eb95f6db4b4c060fd19e85edf96e2550fe
Matin-Modarresi <modarresimatin@gmail.com>
2026-05-22 22:05:36 +0330

    feat(cart): implement cart item with unique cart-product constraint

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from apps.core.models import TimeStampedModel
+from apps.products.models import Product
 
 
 class Cart(TimeStampedModel):
@@ -17,3 +18,31 @@ class Cart(TimeStampedModel):
     def __str__(self):
         return f"Cart for {self.user.email}"
 
+
+class CartItem(TimeStampedModel):
+
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="cart_items"
+    )
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["cart", "product"],
+                name="unique_cart_product"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
