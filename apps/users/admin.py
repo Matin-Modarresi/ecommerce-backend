eb472c4244336f8432208bc799800d9e065f7ec4
Matin-Modarresi <modarresimatin@gmail.com>
2026-05-26 16:55:53 +0330

    feat((users): add name computed fields with ordering

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 from .models import User, UserProfile
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-
-    list_display = ("id", "email", "is_staff", "is_active")
+    list_display = ("id", "email", "is_staff", "is_active", "name")
     search_fields = ("email",)
+
+    def get_queryset(self, request):
+        return super() \
+            .get_queryset(request) \
+            .annotate(
+            name=Concat(F('first_name'), Value(' '), F('last_name'))
+        )
+
+    @admin.display(ordering='name', description='custom name')
+    def name(self, user):
+        return user.name
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-
     list_display = ("id", "user", "phone_number")
