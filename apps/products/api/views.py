d3fe8bfe0e96df05e618e6046d907f6d7345a13c
Matin-Modarresi <modarresimatin@gmail.com>
2026-05-27 18:51:11 +0330

    feat(products): add custom pagination, search and ordering filters to ProductViewSet

--- apps/products/api/views.py
+++ apps/products/api/views.py
@@ -1,15 +1,29 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from ..models import Product
 from .serializers import ProductListSerializer, ProductDetailSerializer
 from .filters import ProductFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from .pagination import ProductDefaultPagination
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.active.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = ProductFilter
+
     permission_classes = [permissions.AllowAny]
+    pagination_class = ProductDefaultPagination
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    filterset_class = ProductFilter
+
+    search_fields = ['name', 'description']
+
+    ordering_fields = ['price', 'crated_at', 'average_rating']
+    ordering = ['-created_at']
 
     def get_serializer_class(self):
         if self.action == 'list':
